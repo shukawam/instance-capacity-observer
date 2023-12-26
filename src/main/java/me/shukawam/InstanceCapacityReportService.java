59359daa78067c1c3993a395a5a8c8b5fee4aa93
shukawam <shukawam@gmail.com>
2023-12-26 19:10:43 +0900

    Support faultDomain

--- src/main/java/me/shukawam/InstanceCapacityReportService.java
+++ src/main/java/me/shukawam/InstanceCapacityReportService.java
@@ -20,9 +20,11 @@ import com.oracle.bmc.core.model.CreateComputeCapacityReportDetails;
 import com.oracle.bmc.core.requests.CreateComputeCapacityReportRequest;
 import com.oracle.bmc.identity.IdentityClient;
 import com.oracle.bmc.identity.requests.ListAvailabilityDomainsRequest;
+import com.oracle.bmc.identity.requests.ListFaultDomainsRequest;
 import com.oracle.bmc.identity.requests.ListRegionSubscriptionsRequest;
 
 import io.helidon.microprofile.scheduling.FixedRate;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -50,25 +52,30 @@ public class InstanceCapacityReportService {
     }
 
     @FixedRate(initialDelay = 5, value = 10, timeUnit = TimeUnit.MINUTES)
+    @WithSpan(value = "generate_capacity_report_metrics")
     public void generateCapacityReportMetrics() {
         LOGGER.info("Generate compute capacity report.");
         var regions = listRegions();
         regions.forEach(region -> {
             initializeClient(region);
-            var adIds = listAvailabilityDomainsId();
-            adIds.forEach(ad -> {
-                LOGGER.fine(String.format("Availability Domain: %s", ad));
-                var capacityReport = createComputeCapacityReportResponse(ad);
-                capacityReport.getShapeAvailabilities().forEach(availability -> {
-                    var status = availability.getAvailabilityStatus().getValue();
-                    LOGGER.info(String.format("Shape %s is %s.", availability.getInstanceShape(), status));
-                    var tags = new ArrayList<Tag>();
-                    tags.add(new Tag("availability_domain", ad));
-                    tags.add(new Tag("fault_domain", availability.getFaultDomain()));
-                    tags.add(new Tag("shape", availability.getInstanceShape()));
-                    tags.add(new Tag("status", availability.getAvailabilityStatus().getValue()));
-                    metricRegistry.gauge("gpu_shape_status", () -> getAvailableCount(availability),
-                            tags.toArray(new Tag[tags.size()]));
+            var availabilityDomains = listAvailabilityDomains();
+            availabilityDomains.forEach(availabilityDomain -> {
+                LOGGER.fine(String.format("Availability Domain: %s", availabilityDomain));
+                var faultDomains = listFaultDomains(availabilityDomain);
+                faultDomains.forEach(faultDomain -> {
+                    var capacityReport = createComputeCapacityReportResponse(availabilityDomain, faultDomain);
+                    capacityReport.getShapeAvailabilities().forEach(availability -> {
+                        var status = availability.getAvailabilityStatus().getValue();
+                        LOGGER.info(String.format("%s %s %s >> %s", availability.getInstanceShape(), availabilityDomain,
+                                faultDomain, status));
+                        var tags = new ArrayList<Tag>();
+                        tags.add(new Tag("availability_domain", availabilityDomain));
+                        tags.add(new Tag("fault_domain", faultDomain));
+                        tags.add(new Tag("shape", availability.getInstanceShape()));
+                        tags.add(new Tag("status", availability.getAvailabilityStatus().getValue()));
+                        metricRegistry.gauge("gpu_shape_status", () -> getAvailableCount(availability),
+                                tags.toArray(new Tag[tags.size()]));
+                    });
                 });
             });
         });
@@ -89,40 +96,60 @@ public class InstanceCapacityReportService {
         return regions;
     }
 
-    private List<String> listAvailabilityDomainsId() {
-        var adIds = new ArrayList<String>();
+    private List<String> listAvailabilityDomains() {
+        var availabilityDomains = new ArrayList<String>();
         var response = identityClient
                 .listAvailabilityDomains(ListAvailabilityDomainsRequest.builder().compartmentId(compartmentId).build());
         if (response.get__httpStatusCode__() != 200) {
             LOGGER.info(String.format("Status code: %s", response.get__httpStatusCode__()));
             throw new ServiceException("Something wrong.");
         }
-        response.getItems().forEach(ad -> {
-            LOGGER.fine(ad.getName());
-            adIds.add(ad.getName());
+        response.getItems().forEach(availabilityDomain -> {
+            LOGGER.fine(availabilityDomain.getName());
+            availabilityDomains.add(availabilityDomain.getName());
         });
-        return adIds;
+        return availabilityDomains;
     }
 
-    private ComputeCapacityReport createComputeCapacityReportResponse(String availabilityDomain) {
+    private List<String> listFaultDomains(String ad) {
+        var faultDomains = new ArrayList<String>();
+        var response = identityClient.listFaultDomains(
+                ListFaultDomainsRequest.builder().compartmentId(compartmentId).availabilityDomain(ad).build());
+        if (response.get__httpStatusCode__() != 200) {
+            LOGGER.info(String.format("Status code: %s", response.get__httpStatusCode__()));
+            throw new ServiceException("Something wrong.");
+        }
+        response.getItems().forEach(fd -> {
+            LOGGER.fine(String.format("FD: %s", fd.getName()));
+            faultDomains.add(fd.getName());
+        });
+        return faultDomains;
+    }
+
+    private ComputeCapacityReport createComputeCapacityReportResponse(String availabilityDomain, String faultDomain) {
         var request = CreateComputeCapacityReportRequest.builder()
-                .createComputeCapacityReportDetails(CreateComputeCapacityReportDetails.builder()
-                        .availabilityDomain(availabilityDomain).compartmentId(compartmentId)
-                        .shapeAvailabilities(new ArrayList<>(Arrays.asList(
-                                CreateCapacityReportShapeAvailabilityDetails
-                                        .builder().instanceShape("BM.GPU.H100.8")
-                                        .instanceShapeConfig(CapacityReportInstanceShapeConfig.builder().ocpus(112.0f)
-                                                .memoryInGBs(2048.0f).build())
-                                        .build(),
-                                CreateCapacityReportShapeAvailabilityDetails.builder().instanceShape("BM.GPU.A100-v2.8")
-                                        .instanceShapeConfig(CapacityReportInstanceShapeConfig.builder().ocpus(128.0f)
-                                                .memoryInGBs(2048.0f).build())
-                                        .build(),
-                                CreateCapacityReportShapeAvailabilityDetails.builder().instanceShape("BM.GPU4.8")
-                                        .instanceShapeConfig(CapacityReportInstanceShapeConfig.builder().ocpus(64.0f)
-                                                .memoryInGBs(2048.0f).build())
-                                        .build())))
-                        .build())
+                .createComputeCapacityReportDetails(
+                        CreateComputeCapacityReportDetails
+                                .builder().availabilityDomain(
+                                        availabilityDomain)
+                                .compartmentId(compartmentId)
+                                .shapeAvailabilities(new ArrayList<>(Arrays.asList(
+                                        CreateCapacityReportShapeAvailabilityDetails.builder()
+                                                .instanceShape("BM.GPU.H100.8").faultDomain(faultDomain)
+                                                .instanceShapeConfig(CapacityReportInstanceShapeConfig.builder()
+                                                        .ocpus(112.0f).memoryInGBs(2048.0f).build())
+                                                .build(),
+                                        CreateCapacityReportShapeAvailabilityDetails.builder()
+                                                .instanceShape("BM.GPU.A100-v2.8").faultDomain(faultDomain)
+                                                .instanceShapeConfig(CapacityReportInstanceShapeConfig.builder()
+                                                        .ocpus(128.0f).memoryInGBs(2048.0f).build())
+                                                .build(),
+                                        CreateCapacityReportShapeAvailabilityDetails.builder()
+                                                .instanceShape("BM.GPU4.8").faultDomain(faultDomain)
+                                                .instanceShapeConfig(CapacityReportInstanceShapeConfig.builder()
+                                                        .ocpus(64.0f).memoryInGBs(2048.0f).build())
+                                                .build())))
+                                .build())
                 .build();
         var createComputeCapacityReportResponse = computeClient.createComputeCapacityReport(request);
         if (createComputeCapacityReportResponse.get__httpStatusCode__() != 200) {
@@ -139,12 +166,11 @@ public class InstanceCapacityReportService {
 
     private Long getAvailableCount(CapacityReportShapeAvailability availability) {
         var count = availability.getAvailableCount();
-        LOGGER.info(String.format("Available count: ", count));
+        LOGGER.info(String.format("Available count: %s", count));
         if (count == null) {
             return Long.valueOf(0);
         } else {
             return count;
         }
-
     }
 }
